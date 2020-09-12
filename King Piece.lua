e17506690d5492a5a0aa00183f9edf1bcf7f2fba
Donut3339 <39208955+Donut3339@users.noreply.github.com>
2020-09-12 14:12:14 +0700

    Update King Piece.lua

--- King Piece.lua
+++ King Piece.lua
@@ -25,19 +25,23 @@ else
 end
 
 -- Local Variable --
+local UIS = game:service'UserInputService'
 local Players = game:service'Players'
 local LocalP = Players.LocalPlayer
 
 -- Global Variable --
 Enabled = false
 Disabled = false
+Quest = false
 TpDF = false
-Distance = -12
+Distance = 5.8
 SkillZ = false
 SkillX = false
 SkillC = false
 SkillV = false
 SkillB = false
+wSpeed = false
+jPower = false
 
 -- NoClip --
 Stepped = game:service'RunService'.RenderStepped:Connect(function()
@@ -73,14 +77,18 @@ s2:Cheat("Label", "Discord Server (discord.gg/fGHVnPN)")
 -- Main Sector --
 local s1 = c1:Sector("AutoFarm")
 local s2 = c1:Sector("Misc")
+local s3 = c1:Sector("Tool Skill")
+local s4 = c1:Sector("Local")
 
 -- Mobs Dropdown --
 local mobs = s1:Cheat("Dropdown", "Mobs", function(SelectedOption)
     SelectedMob = SelectedOption
+    wait()
+    updateMobList()
 end, {options = {}, default = ""})
 
 -- Remove value duplicated in table/array --
-local function removeDuplicates(arr)
+function removeDuplicates(arr)
     local newArray = {}
     local checkerTbl = {}
     for _, element in ipairs(arr) do
@@ -95,14 +103,73 @@ end
 -- Get Mobs and then insert to table/array --
 mobA = {}
 for _,mob in pairs(game:service'ReplicatedStorage'["MOB"]:GetChildren()) do
-    if mob:IsA("Model") and mob:FindFirstChild("Humanoid") and mob:FindFirstChild("HumanoidRootPart") then
+    if mob:IsA("Model") and mob:FindFirstChild("Humanoid") and mob:FindFirstChild("HumanoidRootPart") and string.find(mob.Name, "(%d)>(1000)") then
         table.insert(mobA, mob.Name)
     end
 end
 
+
+-- Add to list option after Duplicated value removed --
 mobB = removeDuplicates(mobA)
 for i = 1, #mobB do
     mobs:AddOption(mobB[i])
+end
+
+function updateMobList()
+    for i = 1, #mobB do
+        mobs:RemoveOption(questB[i])
+    end
+    wait()
+    mobA = {}
+    for _,mob in pairs(game:service'ReplicatedStorage'["MOB"]:GetChildren()) do
+        if mob:IsA("Model") and mob:FindFirstChild("Humanoid") and mob:FindFirstChild("HumanoidRootPart") and string.find(mob.Name, "(%d)>(1000)") then
+            table.insert(mobA, mob.Name)
+        end
+    end
+
+    mobB = removeDuplicates(mobA)
+    for i2 = 1, #mobB do
+        mobs:AddOption(mobB[i2])
+    end
+end
+
+-- Quest Dropdown --
+local quest = s1:Cheat("Dropdown", "Quest", function(SelectedOption)
+    SelectedQuest = SelectedOption
+    wait(2)
+    updateQuestList()
+end, {options = {}, default = ""})
+
+-- Get Quest Name and then insert to table/array --
+questA = {}
+for _,quest in ipairs(workspace.AntiTPNPC:GetChildren()) do
+    if quest:IsA("Model") and string.find(quest.Name, 'QuestLvl%d') then
+        table.insert(questA, quest.Name)
+    end
+end
+
+-- Add to list option after Duplicated value removed --
+questB = removeDuplicates(questA)
+for i = 1, #questB do
+    quest:AddOption(questB[i])
+end
+
+function updateQuestList()
+    for i = 1, #questB do
+        quest:RemoveOption(questB[i])
+    end
+    wait()
+    questA = {}
+    for _,quest in pairs(workspace.AntiTPNPC:GetChildren()) do
+        if quest:IsA("Model") and string.find(quest.Name, 'QuestLvl%d') then
+            table.insert(questA, quest.Name)
+        end
+    end
+
+    questB = removeDuplicates(questA)
+    for i2 = 1, #questB do
+        quest:AddOption(questB[i2])
+    end
 end
 
 -- Tool's Dropdown --
@@ -126,26 +193,26 @@ local distances = s1:Cheat("Textbox", "Distance", function(value)
 end, {placeholder = "Distance"})
 
 -- Enabled Checkbox --
-local enabled = s1:Cheat("Checkbox", "Enabled", function(state)
+local enabled = s1:Cheat("Checkbox", "Auto-Farm", function(state)
     Enabled = state
     while Enabled do
         if Enabled and not Disabled then
             for i,v in pairs(game:service'ReplicatedStorage'["MOB"]:GetChildren()) do
                 if Enabled and not Disabled and v:IsA("Model") and v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") and v.Humanoid.Health > 0 and v.Name == SelectedMob then
-                     if Enabled and not Disabled then
+                    if Enabled and not Disabled then
                         repeat
                             pcall(function()
-                                LocalP.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame + Vector3.new(0, Distance, 0)
+                                LocalP.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame * CFrame.Angles(math.rad(-90),0,0) + Vector3.new(0, Distance, 0)
                             end)
                             wait()
                             pcall(function()
                                 if LocalP:FindFirstChild("Backpack"):FindFirstChild(SelectedWeapon) then
                                     local tool = LocalP:WaitForChild("Backpack"):FindFirstChild(SelectedWeapon)
-                                    wait(.4)
                                     LocalP.Character.Humanoid:EquipTool(tool)
                                 end
                                 if LocalP.Character:FindFirstChildOfClass("Tool").Equipped then
                                     local tool = LocalP.Character:FindFirstChildOfClass("Tool")
+                                    tool:Activate()
                                     if tool:FindFirstChildOfClass("RemoteEvent") then
                                         for _,event in pairs(tool:GetChildren()) do
                                             if event:IsA("RemoteEvent") then
@@ -180,33 +247,106 @@ local enabled = s1:Cheat("Checkbox", "Enabled", function(state)
                     end
                 end
             end
+            wait()
         end
     end
 end)
 
+local autoquest = s1:Cheat("Checkbox", "Auto-Quest", function(state)
+    Quest = state
+    local QuestProgress = LocalP:FindFirstChild("QuestProgress")
+    local CurrentQuest = LocalP:FindFirstChild("CurrentQuest")
+    local Char = LocalP.Character
+    while Quest and wait(1) do
+        if CurrentQuest.Value == "" and not Disabled then
+            if workspace.AntiTPNPC:FindFirstChild(SelectedQuest) then
+                local Head = workspace.AntiTPNPC:FindFirstChild(SelectedQuest).Head
+                Char.Humanoid:ChangeState(11)
+                Char.HumanoidRootPart.CFrame = Head.CFrame + Vector3.new(0, 2, 0)
+                -- mousemoveabs(x, y)
+                -- mouse1click()
+                wait(2)
+                if LocalP.PlayerGui:FindFirstChild(SelectedQuest) then
+                    local quest = LocalP.PlayerGui:FindFirstChild(SelectedQuest)
+                    if quest.Enabled == true then
+                        local acceptButton = quest.Dialogue:FindFirstChild("Accept")
+                        for i2,v2 in pairs(getconnections(acceptButton.MouseButton1Click)) do
+                            v2:Fire()
+                            -- mousemoveabs(x, y)
+                            -- mouse1click()
+                            wait()
+                        end
+                    end
+                end
+            end
+        end
+        wait(2)
+    end
+end)
+
+--[[
+    Sector 3 / Skill Sector
+]]
+
 -- AutoSkillZ Checkbox --
-local z = s1:Cheat("Checkbox", "Z", function(state)
+local z = s3:Cheat("Checkbox", "Z", function(state)
     SkillZ = state
 end)
 
 -- AutoSkillX Checkbox --
-local x = s1:Cheat("Checkbox", "X", function(state)
+local x = s3:Cheat("Checkbox", "X", function(state)
     SkillX = state
 end)
 
 -- AutoSkillC Checkbox --
-local c = s1:Cheat("Checkbox", "C", function(state)
+local c = s3:Cheat("Checkbox", "C", function(state)
     SkillC = state
 end)
 
 -- AutoSkillV Checkbox --
-local v = s1:Cheat("Checkbox", "V", function(state)
+local v = s3:Cheat("Checkbox", "V", function(state)
     SkillV = state
 end)
 
 -- AutoSkillB Checkbox --
-local b = s1:Cheat("Checkbox", "B", function(state)
+local b = s3:Cheat("Checkbox", "B", function(state)
     SkillB = state
+end)
+
+--[[
+    Sector 4 / Local Sector
+]]
+
+-- Walkspeed value slider --
+local wsVal = s4:Cheat("Slider", "Walkspeed Value", function(Value)
+    speedVal = Value
+end, {min = 16, max = 200, suffix = ""})
+
+-- Jumppower value slider --
+local jpVal = s4:Cheat("Slider", "JumpPower Value", function(Value)
+    jpValue = Value
+end, {min = 16, max = 200, suffix = ""})
+
+-- WalkSpeed Checkbox --
+local ws = s4:Cheat("Checkbox", "Walkspeed", function(state)
+    wSpeed = state
+    if wSpeed then
+        if speedVal == nil then speedVal = 30 end
+        LocalP.Character.Humanoid.WalkSpeed = speedVal
+    else
+        LocalP.Character.Humanoid.WalkSpeed = 16  
+    end
+end)
+
+-- JumpPower Checkbox --
+local ws = s4:Cheat("Checkbox", "JumpPower", function(state)
+    jPower = state
+    if jPower then
+        if jpValue == nil then jpValue = 30 end
+        LocalP.Character.Humanoid.JumpPower = jpValue
+    else
+        LocalP.Character.Humanoid.JumpPower = 50
+    end
 end)
 
 -- Devil Fruit TP --
